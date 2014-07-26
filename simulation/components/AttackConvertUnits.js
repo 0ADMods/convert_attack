0c715898f9c9201198f7bef9e5d06a7052d88fdd
J.R.I.B.-Wein <Radagast@dragontale.de>
2014-07-27 00:04:11 +0200

    Fix turnLength error. Rework conversion. Add capturing differentiation.
    Adapt PerformAttack() to new turnLength scope of 0AD.
    Add unfinished capture criteria.
    Differentiate capturing inbetween making a prisoner of war and a direct conversion.

--- simulation/components/AttackConvertUnits.js
+++ simulation/components/AttackConvertUnits.js
@@ -87,6 +87,8 @@ Attack.prototype.PerformAttack = function(type, target)
 	// If this is a ranged attack, then launch a projectile
 	if (type == "Ranged")
 	{
+		var cmpTimer = Engine.QueryInterface(SYSTEM_ENTITY, IID_Timer);
+		var turnLength = cmpTimer.GetLatestTurnLength() / 1000;
 		// In the future this could be extended:
 		//  * Obstacles like trees could reduce the probability of the target being hit
 		//  * Obstacles like walls should block projectiles entirely
@@ -112,7 +114,7 @@ Attack.prototype.PerformAttack = function(type, target)
 		var relativePosition = Vector3D.sub(targetPosition, selfPosition);
 		var previousTargetPosition = Engine.QueryInterface(target, IID_Position).GetPreviousPosition();
 
-		var targetVelocity = Vector3D.sub(targetPosition, previousTargetPosition).div(this.turnLength);
+		var targetVelocity = Vector3D.sub(targetPosition, previousTargetPosition).div(turnLength);
 		// the component of the targets velocity radially away from the archer
 		var radialSpeed = relativePosition.dot(targetVelocity) / relativePosition.length();
 
@@ -155,21 +157,47 @@ Attack.prototype.PerformAttack = function(type, target)
 		var cmpTimer = Engine.QueryInterface(SYSTEM_ENTITY, IID_Timer);
  		cmpTimer.SetTimeout(this.entity, IID_Attack, "MissileHit", timeToTarget*1000, {"type": type, "target": target, "position": realTargetPosition, "direction": missileDirection, "projectileId": id, "playerId":playerId});
     }
-	// TODO: improve convert
 	else if (type == "Convert")
 	{
         
 		var cmpOwnership = Engine.QueryInterface(target, IID_Ownership);
 		if (!cmpOwnership)
 			return;
-        warn('Owner Target: ' + cmpOwnership);
+        //warn('Owner Target: ' + cmpOwnership);
 
 		var cmpOwnership2 = Engine.QueryInterface(this.entity, IID_Ownership);
 		if (!cmpOwnership2)
 			return;
-        warn('Owner Source: ' + cmpOwnership2);
+        //warn('Owner Source: ' + cmpOwnership2);
 
-		cmpOwnership.SetOwner(cmpOwnership2.GetOwner());
+		var isImmediatelyIntegrated = true;
+		var cmpUnitAi = Engine.QueryInterface(this.entity, IID_UnitAI);
+		if (cmpUnitAi.CanCapture(target))
+		{
+			if (isImmediatelyIntegrated)
+			{
+				// Fully convert to a normal unit of your own, the original ethnicity still recognizable.
+				cmpOwnership.SetOwner(cmpOwnership2.GetOwner());
+				warn('Unit ' + this.entity + ' (Owner: '+ cmpOwnership +') immediately integrated target: ' + target + ' (Owner: '+ cmpOwnership2 +' ).');
+			}
+			else
+			{
+				var cmpTargetUnitAi = Engine.QueryInterface(target, IID_UnitAI);
+				if (cmpTargetUnitAi) 
+				{
+					// Take prisoner of war (make it either a prisoner, i.e. garrison or keep it with guards, or a slave worker). Only change accessories or clothes.
+					// TODO Trigger guard function: i.e. make the captives/slaves guard their capturer.
+					if (cmpTargetUnitAi.isGuardOf())
+						cmpTargetUnitAi.RemoveGuard();
+					cmpTargetUnitAi.UnitFsmSpec["Order.Guard"]({ target:target_entity });
+					// TODO Change Actor or add slave robe or adapt other props.
+					 
+				}
+			}
+			Engine.PostMessage(target, MT_OwnershipChanged, { "entity": this.entity });
+		}		
+		else 
+			warn("Can't capture: " + target);
 
 	}
 	else
@@ -208,5 +236,115 @@ Attack.prototype.GetNearbyEntities = function(startEnt, range, friendlyFire)
 }
 */
 
+UnitAI.prototype.CanCapture = function(target)
+{
+    // The healthier the capturing unit the easier for it to capture a target. 
+	var cmpHealth = Engine.QueryInterface(this.entity, IID_Health);
+	if (!cmpHealth)// || cmpHealth.IsUnhealable()) //<-- include this right side or in the formula.
+        return false;
+    var health_normalized = cmpHealth.GetHitpoints() / cmpHealth.GetMaxHitpoints();
+
+    // The healthier a unit the more difficult to capture it. 
+	// Verify that the target is not at max health. Then a unit has to come pretty close: 
+    // TODO replace with formula that takes distance into account. Include all enemy units.
+	var target_cmpHealth = Engine.QueryInterface(target, IID_Health);
+	if (!target_cmpHealth)// || cmpHealth.IsUnhealable()) //<-- include this right side or in the formula.
+        return false;
+    var target_health_normalized = target_cmpHealth.GetHitpoints() / target_cmpHealth.GetMaxHitpoints();
+
+    // Nearby units get captured easier.
+    var distance = DistanceBetweenEntities(this.entity, target);
+/*TODO  Get distance for every unit close by. If performance allows.
+ * var thisCmpPosition = Engine.QueryInterface(this.entity, IID_Position);
+	var s = thisCmpPosition.GetPosition();
+
+	var t = targetCmpPosition.GetPosition();
+
+	var h = s.y-t.y+range.elevationBonus;
+	var maxRangeSq = 2*range.max*(h + range.max/2);
+
+	if (maxRangeSq < 0)
+		return false;
+
+	var cmpUnitMotion = Engine.QueryInterface(this.entity, IID_UnitMotion);
+	return cmpUnitMotion.IsInTargetRange(target, range.min, Math.sqrt(maxRangeSq));
+	return maxRangeSq >= distanceSq && range.min*range.min <= distanceSq;
+*/
+    var this_moveDirection_normalized = 1 ;//cmpUnitMotion.GetDirection(); //or determine yourself from last and this location.
+    var target_moveDirection_normalized = 1;// assume enemy is not fleeing for now.
+
+
+
+    // Units which are surrounded by many enemies and few friendly units get captured easier.
+/*	var cmpRanged = Engine.QueryInterface(this.entity, iid);
+	if (!cmpRanged)
+		return false;*/
+	//var range = cmpRanged.GetRange(type);
+    //GetUnitsInRange(); TODO
+    var friendlyToEnemyWithinRangeRatio = 1;
+
+    // fleeing units get captured more easily.
+	var cmpUnitMotion = Engine.QueryInterface(this.entity, IID_UnitMotion);
+    var easyCatchBonus = 0;
+    if (target_moveDirection_normalized == -1 * this_moveDirection_normalized) { 
+	    //return cmpUnitMotion.IsInTargetRange(target, range.min, range.max);
+        easyCatchBonus = 10;
+    }
+    var base_chance = 50;
+    //warn[(]friendlyToEnemyWithinRangeRatio + ' * distance: ' + distance + '  health_normalized: ' + health_normalized + ' - ' + target_health_normalized + ' target_health_normalized');
+    // Is this all enough to capture the unit and make it prisoner of the unit that captured it? (use Guard function for this in the meantime, but only for units)
+    var chanceForConversionSuccess = friendlyToEnemyWithinRangeRatio * (base_chance + (health_normalized - target_health_normalized) * 100 - distance + easyCatchBonus);
+    /*
+    var captureLuck = 50;
+    var toBeCapturedLuck = 50;
+    if (toBeCapturedLuck > 90) {
+    }
+    */   
+    //warn[(]'Chance for Conversion Success: ' + chanceForConversionSuccess);
+    var chanceIncreaseByRandomLastHopeOppositionBoost = 5; //TODO randomize.
+    var chanceMinimumForConversionSuccess = 30;
+    if (chanceForConversionSuccess < (chanceMinimumForConversionSuccess + chanceIncreaseByRandomLastHopeOppositionBoost)) {
+        return false;
+    }
+    // I had a formula somewhere in the Forum. Have to look for it.
+
+
+
+	// Verify that the target has no unconvertible class (e.g. a Hero?)
+    //
+	// Verify that we're able to respond to Heal commands
+	var cmpHeal = Engine.QueryInterface(this.entity, IID_Heal);
+	if (!cmpHeal) 
+        return false;
+
+    // TODO create those classes (schema + xml). For now use healable classes.
+	var cmpIdentity = Engine.QueryInterface(target, IID_Identity);
+	if (!cmpIdentity)
+		return false;
+	for each (var unhealableClass in cmpHeal.GetUnhealableClasses())
+	{
+		if (cmpIdentity.HasClass(unhealableClass) != -1)
+		{
+			return false;
+		}
+	}
+
+	// Verify that the target is a convertible class:
+	var convertible = false;
+	for each (var convertibleClass in cmpHeal.GetHealableClasses())//GetConvertibleClasses())
+	{
+		if (cmpIdentity.HasClass(convertibleClass) != -1)
+		{
+			convertible = true;
+		}
+	}
+	if (!convertible)
+		return false;
+
+
+    //warn[(]'Conversion was successful.');
+	return true;
+
+};
 
 Engine.ReRegisterComponentType(IID_Attack, "Attack",  Attack);
